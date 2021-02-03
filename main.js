58697a64a3db6e0d4bb0912036e1e48e83580777
Nicolas Wailly <nicolas.wailly.dev@outlook.com>
2021-02-03 20:02:33 +0100

    add cancel|pi|equal buttons logic

--- main.js
+++ main.js
@@ -1,39 +1,75 @@
-let outputOne = document.getElementById("output-one"),
-    outputTwo = document.getElementById("output-two"),
-    btnAction = document.getElementsByClassName("action"),
-    btnNumber = document.getElementsByClassName("number"),
-    reset = document.getElementById("reset"),
-    equal = document.getElementById("equal"),
-    cancel =  document.getElementById("cancel");
+const CALCUL = document.getElementById("calcul"),
+    RESULT = document.getElementById("result"),
+    HISTORY = document.getElementById("history"),
+    BTN_ACTION = document.getElementsByClassName("action"),
+    BTN_NUMBER = document.getElementsByClassName("number"),
+    RESET = document.getElementById("reset"),
+    EQUAL = document.getElementById("equal"),
+    CANCEL =  document.getElementById("cancel"),
+    PI = document.getElementById("pi"),
+    EXP = document.getElementById("exp");
 
 //  add a event on all number button
-for (const item of btnNumber) {
+for (const item of BTN_NUMBER) {
     item.addEventListener("click", (e) => {        
-        outputOne.textContent += e.target.textContent;
+        CALCUL.textContent += e.target.textContent;
     });
 }
 
 //  add a event on all action button
-for (const item of btnAction) {
+for (const item of BTN_ACTION) {
     item.addEventListener("click", (e) => {
-        outputOne.textContent += e.target.textContent;
+        CALCUL.textContent += e.target.textContent;
     });
 }
 
-//  reset button
-reset.addEventListener("click", () => {
-    outputOne.textContent = "";
-    outputTwo.textContent = "";
+//  RESET button
+RESET.addEventListener("click", () => {
+    CALCUL.textContent = "";
+    RESULT.textContent = "";
 });
 
-//  equal button
-equal.addEventListener("click", () =>{
-    outputTwo.textContent = outputOne.textContent + "=";
-    outputOne.textContent = eval(outputOne.textContent);
+//  EQUAL button
+EQUAL.addEventListener("click", () =>{
+    let rgx = /[*\-\+\/]/,
+        calcul = eval(CALCUL.textContent);
+
+    //  test if calcul line is empty or doesn't include calcul operator
+    if(CALCUL.textContent === "" || !rgx.test(CALCUL.textContent)) return;
+    // add calcul to history
+    if(RESULT.textContent != "" && RESULT.textContent != CALCUL.textContent+"=")
+        HISTORY.textContent = RESULT.textContent + eval(RESULT.textContent.substring(0,RESULT.textContent.length-1));
+
+    RESULT.textContent = CALCUL.textContent + "=";
+    CALCUL.textContent = calcul;        
 });
 
-//  cancel button
-cancel.addEventListener("click", () =>{
-    let size = outputOne.textContent.length - 1;
-    outputOne.textContent = outputOne.textContent.slice(0,size);
+//  CANCEL button
+CANCEL.addEventListener("click", () =>{
+    let size = CALCUL.textContent.length - 1;
+    CALCUL.textContent = CALCUL.textContent.slice(0,size);
 });
+
+//  PI button
+PI.addEventListener("click", (e) =>{
+    let operators = ["+","-","*","/"],
+        lastChar = "",
+        isLineEmpty = false;
+
+    if(CALCUL.textContent.length > 0)
+        lastChar = CALCUL.textContent[CALCUL.length-1];
+    else
+        isLineEmpty = true;
+    
+    if(operators.includes(lastChar) || isLineEmpty)
+        CALCUL.textContent += Math.PI;    
+});
+
+// EXP button
+
+
+/**
+ * @param {string} a The first string to add
+ * @param {string} b The second string to add
+ * @return {string} Return the string 'a' + string 'b'
+ */
